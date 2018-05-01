9dfba4c278154e60283453ccfcd31a05b9b49b3e
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-05-01 19:42:42 +0200

    Starting at random speeds, also added ci. exp. (?)

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ const sketch = (p: p5) => {
   var coordinates: p5.Vector[] = [];
   var dimensions: p5.Vector[] = [];
   var speeds: p5.Vector[] = [];
+  var expansionSpeeds: p5.Vector[] = [];
 
   const numberOfCircles = 50;
 
@@ -39,8 +40,10 @@ const sketch = (p: p5) => {
       const coordinate = coordinates[index];
       const dimension = dimensions[index];
       const speed = speeds[index];
+      const expansionSpeed = expansionSpeeds[index];
 
       coordinate.add(speed.x, speed.y);
+      dimension.add(expansionSpeed.x, expansionSpeed.y);
 
       if (coordinate.x >= p.width || coordinate.x <= 0) {
         speed.set(speed.x * -1, speed.y);
@@ -48,6 +51,13 @@ const sketch = (p: p5) => {
 
       if (coordinate.y >= p.height || coordinate.y <= 0) {
         speed.set(speed.x, speed.y * -1);
+      }
+
+      if (
+        dimension.x <= circleDimensions.min ||
+        dimension.x >= circleDimensions.max
+      ) {
+        expansionSpeed.set(expansionSpeed.x * -1, expansionSpeed.y * -1);
       }
 
       p.ellipse(coordinate.x, coordinate.y, dimension.x, dimension.y);
@@ -59,7 +69,14 @@ const sketch = (p: p5) => {
       p.createVector(p.random(0, p.width), p.random(0, p.height)),
     );
 
-    speeds = Array.from(new Array(numberOfCircles), () =>
+    speeds = Array.from(new Array(numberOfCircles), () => {
+      const xAxisSpeed = p.round(p.random(0, 1)) === 1 ? -1 : 1;
+      const yAxisSpeed = p.round(p.random(0, 1)) === 1 ? -1 : 1;
+
+      return p.createVector(xAxisSpeed, yAxisSpeed);
+    });
+
+    expansionSpeeds = Array.from(new Array(numberOfCircles), () =>
       p.createVector(0.5, 0.5),
     );
 
@@ -75,10 +92,10 @@ const sketch = (p: p5) => {
       return p.createVector(firstDimension, firstDimension);
     });
   };
-
-  const mouseClicked = () => {
-    resetCircles();
-  };
 };
 
 const sketchP = new p5(sketch);
+
+const clicked = () => {
+  sketchP();
+};
